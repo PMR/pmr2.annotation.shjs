7d7faf8f37d5de12b659fcda300ac083315542d1
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-04-12 07:48:11 +0000

    Added the new attributes required to keep annotations working.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.annotation.shjs/trunk@4877 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/annotation/shjs/annotator.py
+++ pmr2/annotation/shjs/annotator.py
@@ -5,11 +5,14 @@ from pmr2.app.factory import named_factory
 from pmr2.app.annotation.interfaces import *
 from pmr2.app.annotation.annotator import ExposureFileAnnotatorBase
 
+from pmr2.annotation.shjs.interfaces import ISourceTextNote
+
 
 class SourceTextAnnotator(ExposureFileAnnotatorBase):
     zope.interface.implements(IExposureFileAnnotator)
     title = u'Source Viewer'
     label = u'Source View'
+    for_interface = ISourceTextNote
 
     def generate(self):
         return ()
